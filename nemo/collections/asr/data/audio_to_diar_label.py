ee133af0eaa0a9fd42267e7885e1a00dbe83bfcc
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-30 15:30:00 -0300

    Update audio_to_diar_label.py

--- nemo/collections/asr/data/audio_to_diar_label.py
+++ nemo/collections/asr/data/audio_to_diar_label.py
@@ -34,7 +34,7 @@ from nemo.core.neural_types import (
 from omegaconf import OmegaConf
 from nemo.collections.asr.parts.preprocessing.diarization import LibriSpeechGenerator
 # from nemo.collections.asr.models.msdd_models import get_audio_rttm_map
-from nemo.collections.asr.parts.utils.speaker_utils import write_rttm2manifest, segments_manifest_to_subsegments_manifest
+from nemo.collections.asr.parts.utils.speaker_utils import write_rttm2manifest, segments_manifest_to_subsegments_manifest, get_uniq_id_with_dur
 from nemo.utils import logging
 
 def get_audio_rttm_map(manifest):
