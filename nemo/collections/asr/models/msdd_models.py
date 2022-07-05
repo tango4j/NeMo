3fe7bce4515122657433c00cf66628b1f7af5be7
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-07-04 21:46:10 -0300

    Update msdd_models.py

--- nemo/collections/asr/models/msdd_models.py
+++ nemo/collections/asr/models/msdd_models.py
@@ -516,6 +516,7 @@ class ClusterEmbedding:
 
 class DataLoader(torch.utils.data.dataloader.DataLoader):
     def __init__(self, *args, **kwargs):
+        print(self)
         self.dataset.regenerate_dataset()
         super().__init__(*args, **kwargs)
         print('RELOADED DATALOADER')
