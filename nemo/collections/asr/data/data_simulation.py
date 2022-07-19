e9096308af703adf3fe5df226b634c668e7a1d33
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-07-19 14:40:34 -0300

    Update data_simulation.py

--- nemo/collections/asr/data/data_simulation.py
+++ nemo/collections/asr/data/data_simulation.py
@@ -378,13 +378,6 @@ class LibriSpeechGenerator(object):
                 window = self._get_window(end_window_amount, start=False)
                 self._sentence = torch.cat((self._sentence, np.multiply(audio_file[start_cutoff+prev_dur_sr+release_buffer:start_cutoff+prev_dur_sr+release_buffer+end_window_amount], window)), 0)
 
-        # print('words: ', self._words)
-        # print('alignments: ', self._alignments)
-        # print('start cutoff: ', float(start_cutoff * 1.0 / self._params.data_simulator.sr))
-        # print('len sentence: ', len(self._sentence) * 1.0 / self._params.data_simulator.sr)
-        # print('len audio_file: ', len(audio_file[:prev_dur_sr]) * 1.0 / self._params.data_simulator.sr)
-        # print('len full audio_file: ', len(audio_file) * 1.0 / self._params.data_simulator.sr)
-
         return sentence_duration+nw, len(self._sentence)
 
     # returns new overlapped (or shifted) start position
@@ -632,13 +625,16 @@ class LibriSpeechGenerator(object):
                 array = torch.nn.functional.pad(array, (0, end - len(array)))
             array[start:end] += self._sentence
 
+            print('start: ', start)
             #build entries for output files
             new_rttm_entries = self._create_new_rttm_entry(start / self._params.data_simulator.sr, end / self._params.data_simulator.sr, speaker_ids[speaker_turn])
+            print('new_rttm_entries: ', new_rttm_entries)
             for entry in new_rttm_entries:
                 rttm_list.append(entry)
             new_json_entry = self._create_new_json_entry(os.path.join(basepath, filename + '.wav'), start / self._params.data_simulator.sr, length / self._params.data_simulator.sr, speaker_ids[speaker_turn], self._text, os.path.join(basepath, filename + '.rttm'), os.path.join(basepath, filename + '.ctm'))
             json_list.append(new_json_entry)
             new_ctm_entries = self._create_new_ctm_entry(filename, speaker_ids[speaker_turn], start / self._params.data_simulator.sr)
+            print('new_ctm_entries: ', new_ctm_entries)
             for entry in new_ctm_entries:
                 ctm_list.append(entry)
 
