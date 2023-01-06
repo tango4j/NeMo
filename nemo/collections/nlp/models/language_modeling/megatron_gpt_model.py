a079c356c04fbd36a1458f3a758c6b31125943b9
Sangkug Lym <slym@nvidia.com>
2023-01-05 21:11:14 -0800

    p2p overlap

--- nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_gpt_model.py
@@ -314,6 +314,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             num_micro_batches_with_partial_activation_checkpoints=self.cfg.get(
                 'num_micro_batches_with_partial_activation_checkpoints', None
             ),
+            overlap_p2p_comm=self.cfg.get('overlap_p2p_comm', False),
         )
 
         # only the last stages of the pipeline return losses
@@ -542,6 +543,7 @@ class MegatronGPTModel(MegatronBaseModel, TextGeneration):
             dtype=self.autocast_dtype,
             sequence_parallel_enabled=self.cfg.get('sequence_parallel', False),
             sync_batch_comm=self.cfg.get('sync_batch_comm', False),
+            overlap_p2p_comm=self.cfg.get('overlap_p2p_comm', False),
         )
 
         # only the last stage of the pipeline returns losses
