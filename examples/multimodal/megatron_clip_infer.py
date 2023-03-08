06c8a02c4633b858814a55ced7fd3b523281ad26
yaoyu-33 <yaoyu.094@gmail.com>
2023-03-07 16:18:21 -0800

    Fix clip inference
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- examples/multimodal/megatron_clip_infer.py
+++ examples/multimodal/megatron_clip_infer.py
@@ -66,6 +66,8 @@ def main(cfg) -> None:
     # These configs are required to be off during inference.
     with open_dict(model_cfg):
         model_cfg.precision = cfg.trainer.precision
+        if trainer.precision != "bf16":
+            model_cfg.megatron_amp_o2 = False
         model_cfg.sequence_parallel = False
         model_cfg.activations_checkpoint_granularity = None
         model_cfg.activations_checkpoint_method = None
