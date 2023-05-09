e33f6b1f1cb6306f443cb8915c244f327e4adf10
stevehuang52 <heh@nvidia.com>
2023-05-09 15:13:43 -0400

    add partial transcribe for rnnt
    Signed-off-by: stevehuang52 <heh@nvidia.com>

--- scripts/chime7/evaluation/run_eval.sh
+++ scripts/chime7/evaluation/run_eval.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pred_dir="nemo_experiments/nemo_ctc/"
+# pred_dir="nemo_experiments/nemo_chime6_ft_ctc/"
+pred_dir="nemo_experiments/nemo_chime6_ft_rnnt/"
+
 chime7_dir="/media/data2/chime7-challenge/datasets/chime7_official_cleaned"  
 python evaluate_nemo_asr.py \
     --hyp_folder $pred_dir \
